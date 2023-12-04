b60a8607891f41804b00d5d9932356ede3f09d9c
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-12-04 13:33:32 +0000

    day 4

--- 2023/day-04/main.py
+++ 2023/day-04/main.py
@@ -10,11 +10,10 @@ import numpy as np
 # Find the score of each card and submit the sum of all the scores
 
 
-filename = "test.txt"
+filename = "data.txt"
 f = open(filename, "r")
 cards = [line.strip("\n") for line in f.readlines()]
 
-#print(cards)
 total = 0
 card_number = 0
 card_matches = []
@@ -32,42 +31,33 @@ for c in cards:
     total += score
 
 print("Part 1 total score:", total)
-#print(card_matches)
 
-# PART 1 answer: 21485
+# PART 1 test answer: 13
+# PART 1 data answer: 21485
 
 # --------------- PART 2 --------------
 # Count total number of cards after processing:
-# A card with n matches creates copies the next n cards
+# A card with n matches creates copies of the next n cards
 # (so copies can only be of card numbers greater than the spawning card number)
 # The number of matches dictates how many subsequent cards are copied
 # The current count of the card in question dictates how many copies of each subsequent card to make
 
 index = np.arange(1, len(cards)+1)
-#print(f"Index: {index}")
-#print(f"Card matches: {card_matches}")
 card_count = np.array([1] * len(index)) # start with 1 instance of each card
-#print(f"Sart card count: {card_count}")
-#print(card_count.shape)
 
 for i, cm in zip(index, card_matches):
     # number of cards
     nc = len(index)
     # for each card update the card count
     # next cm cards are incremented by the current card count value
-    # current card count
+    # cc: current card count
     cc = card_count[i-1]
-    #print(f"current card count: {cc}")
+    # create array to match card_count size
     count_update = np.array(i*[0] + cm*[cc] + (nc-cm-i)*[0])
-    #print(f"Current count update to apply: {count_update}")
-    # apply count_update
-    card_count = card_count + count_update
-    print(f"Current card count: {card_count}")
+    # add count_update
+    card_count += count_update
 
-
-
-#print(f"Final card count: {card_count}")
-print(f"Total card count Part 2: {sum(card_count)}")
+print(f"Part 2 total card count: {sum(card_count)}")
 
 # Part 2 test answer: 30
 # Part 2 data answer: 11024379

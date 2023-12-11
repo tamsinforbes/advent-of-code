1086723c46ba1f45c4cbb4c60029c759a4a7c8bf
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-12-11 19:58:58 +0000

    day 10 👹

--- 2023/day-10/main.py
+++ 2023/day-10/main.py
@@ -5,7 +5,7 @@ import numpy as np
 # ---------------- PART 1 ---------------
 # What is the furthest step count from the start along the loop
 
-filename = "test7.txt"
+filename = "data.txt"
 
 # get data into array first (don't put this inside a loop please)
 data = np.genfromtxt(
@@ -94,9 +94,67 @@ def get_next_from_start(arr, start_marker="S"):
             came_from =to_from[p[0]]
             return (start[0][0], start[0][1]), next_loc, came_from
 
+def get_s_pipe(arr, start=(0,0)):
+    """
+    Get the type of pipe hidden under the "S" start position
+    Input:
+        arr: array
+        start: position of start of pipe
+    Output:
+        start_pipe: character representing type of pipe under
+            start marker.
+    """
+    valid_pipe_map = {
+        "N":["7","|","F"],
+        "S":["J","|","L"],
+        "W":["L","-","F"],
+        "E":["7","-","J"]
+    }
+    dir_to_pipe = {
+        "SE": "F", "SW": "7", "NE": "L", "NW": "J", "NS": "|", "WE": "-",
+        "ES": "F", "WS": "7", "EN": "L", "WN": "J", "SN": "|", "EW": "-",
+    }
+
+    # Get positions around start
+    cross_mask = np.array([(-1,0), (0,-1), (1,0), (0,1)]) # N W S E
+    # Add start position to mask
+    positions = start + cross_mask
+    # Add position label
+    labels = ["N", "W", "S", "E"]
+
+    # Label and drop positions outside array
+    positions_lab = []
+    for label, p in zip(labels, positions):
+        row = p[0]
+        col = p[1]
+        n_rows = arr.shape[0]
+        n_cols = arr.shape[1]
+        pipe = arr[row, col]
+        if (0 <= row < n_rows) and (0 <= col < n_cols):
+            positions_lab.append((label, (row, col), pipe))
+    
+    # Drop invalid pipes
+    valid_positions = []
+    for p in positions_lab:
+        pipe = arr[p[1]]
+        if pipe in valid_pipe_map[p[0]]:
+            valid_positions.append(p)
+    
+    # Check exactly two valid_positions
+    # Again get pipe type
+    if len(valid_positions) == 2:
+        dirs = "".join([valid_positions[0][0], valid_positions[1][0]])
+        start_pipe = dir_to_pipe[dirs]
+    else:
+        print(f"Number of valid pipes next to start not exactly equal to 2!")
+
+    return start_pipe
 
 
-# ---------- RUN -----------
+
+
+
+# ---------- PART 1 RUN -----------
 start, current_loc, came_from = get_next_from_start(data, "S")
 print(f"Start values: S location: {start}\nNext pipe location: {current_loc}; Came from: {came_from}")
 
@@ -110,100 +168,109 @@ print(f"Part 1 with {filename}: Total steps: {step}; Furthest number of steps fr
 # Next pipe location: (62, 60); Came from: E
 # Part 1 with data.txt: Total steps: 13630; Furthest number of steps from start: 6815.0
 
+
+
+# ---------------------------------------
 # ---------------- PART 2 ---------------
 # How many tiles are enclosed by the loop?
 # Completely enclosed, like an enclave (ground or other pipe, just not loop)
 
-# Get all the parts of the loop
-loop_tiles = []
+# Get all positions of tiles in the loop
+loop_positions = []
 start, current_loc, came_from = get_next_from_start(data, "S")
-loop_tiles.append(start)
+loop_positions.append(start)
 
 while current_loc != start:
-    loop_tiles.append(current_loc)
+    loop_positions.append(current_loc)
     current_loc, came_from = next_pipe(data, current_loc, came_from)
 
+# Get start pipe type and all the other loop pipes
+start, current_loc, came_from = get_next_from_start(data, "S")
+start_pipe = get_s_pipe(data, start)
+loop_pipes = [start_pipe]
+for pos in loop_positions[1:]:
+    loop_pipes.append(data[pos])
+
+for pos, pipe in zip(loop_positions, loop_pipes):
+    print(pos, pipe)
+
 # Create new array of just the loop and not the loop
-raster = np.zeros(data.shape)
-for tile in loop_tiles:
-    raster[tile] = 1
-print(f"Raster dimnesions: {raster.shape}\n{raster}")
+loop = np.zeros(data.shape, dtype=object)
+for pos, pipe in zip(loop_positions, loop_pipes):
+    loop[pos] = pipe
+# Swap zeros for dots
+loop[loop==0] = "."
+print(f"loop dimnesions: {loop.shape}\n{loop}")
 
-# Create new array of just the pipe tiles
-pipe_arr = np.chararray(data.shape)
-for tile in loop_tiles:
-    pipe_arr[tile] = data[tile]
-print(f"\n{pipe_arr}")
+# Loop properties
+# Reading from the left 
+# Of the 6 pipes: F L J 7 | -
+# F L are vert to horiz:
+# J 7 are horiz to vert
+# | vert to veet
+# - horiz to horiz
+# F, L, are always first in a row containing horizontal wall
+# - is always in the middle of other horizontal pipes
+# J 7 are always last in a section of horizontal wall: they NEVER start (from the left)
+# | is always in the middle of other vertical pipes
 
-
-
-# Raster scan an update wether in or out of the loop
-# for r in raster:
-#     for c in r:
-#         print(c)
-
-state = 0
+# Scan horizontally
 inner_count = 0
-tiles_scanned = 0
-for r in raster:
-    # count 1s in row
-    ones_in_row = sum(r)
-    print(f"Ones in row: {ones_in_row}")
-    # state = 0
-    # for c in r:
-    #     tiles_scanned += 1
-    #     c_int = np.round(c)
-    #     print(c)
-    #     print(c == 0)
-    #     print(state == 0)
-    #     print(np.round(c) == 1)
+for r in loop:
+    # # Keep track of horizonal wall
+    # wall = 0
+    # Keep track of in/outsde loop
+    state = 0
+    # Keep track of previous corner pipe
+    last_corner = "X"
+    print(f"Running count: {inner_count}")
+    for c in r:
+        if c == "|":
+            if state == 0:
+                state = 1
+                print(f"{c} inside")
+            elif state == 1:
+                state = 0
+                print(f"{c} outside")
+        elif c in ["F", "L"]:
+            if state == 0:
+                state = 1
+                last_corner = c
+                print(f"{c} inside")
+            elif state == 1:
+                state = 0
+                last_corner = c
+                print(f"{c} outside")
+        elif c == "7":
+            if last_corner == "F":
+                if state == 0:
+                    state = 1
+                    print(f"{c} inside")
+                elif state == 1:
+                    state = 0
+                print(f"{c} outside")
+            elif last_corner == "L":
+                print(f"{c} No state change")
+        elif c == "J":
+            if last_corner == "L":
+                if state == 0:
+                    state = 1
+                    print(f"{c} inside")
+                elif state == 1:    
+                    state = 0
+                    print(f"{c} outside")
+            elif last_corner == "F":
+                print(f"{c} No state change")
+        elif state == 1 and c == ".":
+            inner_count += 1
+            print(f"{c} Internal dot!!!")
+        elif state == 0 and c == ".":
+            print(f"{c} external dot")
+        elif c == "-":
+            print(f"{c} Stuck in the wall :o(")
+        else:
+            print(f"Uncaptured condition")
 
-    #     if state == 0 and c_int == 1:
-    #         state = 1
-    #         print("inside")
-    #     elif state == 1 and c_int == 0:
-    #         inner_count += 1
-    #     elif state == 1 and c_int == 1:
-    #         state = 0
-    #     elif state == 0 and c_int == 0:
-    #         continue
-
-total_tiles = raster.shape[0] * raster.shape[1]
-print(f"End state: {state}; Inner tile count: {inner_count}")
-print(f"All {total_tiles} tiles scanned: {tiles_scanned == total_tiles} ({tiles_scanned} tiles scanned).")
-# state = 0, c = 0
-# state = 0, c = 1
-# state = 1, c = 0
-# state = 1, c = 1
-
-# Scan each row with a sliding window of 2 tiles
-# rows = raster.shape[0]
-# cols = raster.shape[1]
-
-# state = 0
-# wall = 0
-# total_count = 0
-# tiles_scanned = 0
-# for r in range(rows):
-#     state = 0
-#     for c in range(cols-1):
-#         row_count = 0
-#         tiles_scanned += 1
-#         win = [int(raster[r,c]), int(raster[r, c+1])]
-#         # print(f"Tile: {r, c} window: {win} state: {state} inner count: {inner_count}")
-#         if state == 0 and win == [1,0]:
-#             state = 1
-#         elif state  == 1 and win == [0,0]:
-#             row_count += 1 
-#         elif state == 1 and win == [0,1]:
-
-#         elif state == 1 and win == [0,1]:
-#             state = 0
-#         print(f"Tile: {r, c} window: {win} state: {state} inner count: {row_count}")
-#     total_count += row_count
-
-
-# total_tiles = raster.shape[0] * raster.shape[1]
-# print(f"End state: {state}; Total count: {total_count}")
-# print(f"Scanning sliding window of two tiles.")
-# print(f"All {total_tiles} tiles scanned: {tiles_scanned + cols == total_tiles}")
+print(f"Part 2 with {filename}: Total count of internal dots: {inner_count}")
+            
+# Part 2 with data.txt: Total count of internal dots: 269

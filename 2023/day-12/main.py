32b798b8a00577c9c3043dde6b77cf9a98b3167a
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-12-12 20:18:38 +0000

    part 2 🥺

--- 2023/day-12/main.py
+++ 2023/day-12/main.py
@@ -2,11 +2,12 @@ import exrex
 import numpy as np
 # from itertools import combinations, combinations_with_replacement, permutations
 import re
+import timeit
 # import math
 
 
 # ------------ PART 1 -------------------
-filename = "data.txt"
+filename = "test.txt"
 f = open(filename, "r")
 raw = [d.strip("\n").split(" ") for d in f.readlines()]
 data = [[r[0], [int(i) for i in r[1].split(",")]] for r in raw]
@@ -81,3 +82,23 @@ print(f"\nPart 1 with {filename}: total ways for all rows of springs: {total_way
 
 
 # ----------- PART 2 ------------
+# Now the number get rather larger; time current method to assess feasibilty
+# Each record1  is repeated 5 times with an extra ? (u) in between
+# Each record 2 is repeated 5 times (with a comma separator)
+record1 = "????.######..#####." # len=12
+record2 = [1,6,5]
+# these currently have 10 possibilities, and apparntly have 506250 arrangements after
+# expanding
+exp_record1 = ((record1.replace("?", "u").replace(".","g").replace("#","b") + "u") * 5)[:-1] # drop final u
+exp_record2 = record2 * 5
+print(exp_record1, exp_record2)
+
+tic = timeit.default_timer()
+
+# ways = possible_ways(exp_record1, exp_record2)
+
+toc = timeit.default_timer()
+
+print(f"Time taken to calculate {ways} arrangements: {toc - tic} seconds")
+# Time taken to calculate 2500 arrangements: 74.50608092802577 seconds
+# Time taken to calculate ~500000 arrangements: KILLED 😔
